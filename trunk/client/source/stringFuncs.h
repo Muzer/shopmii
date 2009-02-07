27ca5d75d410adbc91a4a32ce7875057f88d0715
muzerakascooby <muzerakascooby@e3bc4ac6-f2fe-11dd-b507-8b4ecd5210be>
2009-02-07 17:07:35 +0000

    Finally made this compilable :p. Still untested recently though.

---
+++ trunk/client/source/stringFuncs.h
@@ -0,0 +1,23 @@
+/***************************************************************************/
+/* stringFuncs.h - Header file for my common string functions              */
+/*                                                                         */
+/* Copyright (c) 2009 Matt Gentle                                          */
+/*                                                                         */
+/* This program is free software; you can redistribute it and/or modify    */
+/* it under the terms of the GNU General Public License as published by    */
+/* the Free Software Foundation, version 2.                                */
+/*                                                                         */
+/* This program is distributed in the hope that it will be useful,         */
+/* but WITHOUT ANY WARRANTY; without even the implied warranty of          */
+/* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           */
+/* GNU General Public License for more details.                            */
+/*                                                                         */
+/* You should have received a copy of the GNU General Public License       */
+/* along with this program; if not, write to the Free Software             */
+/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA */
+/***************************************************************************/
+
+void trim (char *s);
+void replaceString (char *src, char *change, char *to);
+void subString (char *result, char *src, char *startStr, char *endStr);
+void subStringAlt (char *result, char *src, char *startStr, char *endStr, char *altEndStr);
